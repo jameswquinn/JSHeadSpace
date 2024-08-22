c5ac4d9868feca8c53840646affd478407b8e1b5
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-22 21:42:13 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as HeadSpace } from './HeadSpace';
+export * from './PreactIntegration';
+export * from './ReactIntegration';
+export * from './VueIntegration';
+export * from './SvelteIntegration';
