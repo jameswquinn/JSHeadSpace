626ed83bc7a9517424e9c351cd4feda029a1cc31
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-22 22:34:27 +0100

    Create __layout.svelte

---
+++ examples/svelte-example/src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { HeadSpace } from 'head-space';
+  import { setContext } from 'svelte';
+
+  const headSpace = new HeadSpace();
+  setContext('headSpace', headSpace);
+</script>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/blog/1">Blog Post 1</a>
+  <a href="/blog/2">Blog Post 2</a>
+</nav>
+
+<slot />
