1fb0a3018fdc19dbef2625afe469fc90c9bace8c
jameswquinn <jameswquinn@users.noreply.github.com>
2024-08-22 22:24:36 +0100

    Create App.vue

---
+++ examples/vue-example/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/blog/1">Blog Post 1</router-link> |
+      <router-link to="/blog/2">Blog Post 2</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
